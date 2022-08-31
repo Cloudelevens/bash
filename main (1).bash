6c73e47147b860da6ffa7aad1757a32e8c49135a
bmvij <59024774+bmvij@users.noreply.github.com>
2022-08-31 14:25:41 +0530

    Add files via upload

---
+++ main (1).bash
@@ -0,0 +1,5 @@
+COLORS="red green, blue, yellow"
+for COLOR in $COLORS
+do
+  echo "COLOR: $COLOR"
+done
---
+++ main.bash
@@ -0,0 +1,19 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+MY_SHELL="bash1" 
+
+if [ "$MY_SHELL" = "bash" ]
+
+then 
+  echo " it is bash "
+  
+elif [ "$MY_SHELL" = "bash1" ]
+
+then 
+  echo " it is bash1 "
+ 
+else 
+  echo " it is not bash"
+fi
